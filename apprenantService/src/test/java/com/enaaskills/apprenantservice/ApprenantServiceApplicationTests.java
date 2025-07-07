9b6f1ed856292ab847ac1fd7aa122113f830fc0d
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-07-07 17:52:26 +0100

    création des microservices

---
+++ apprenantService/src/test/java/com/enaaskills/apprenantservice/ApprenantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaaskills.apprenantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
