0bc5c3408eb9934427943fe4e78c726dabc00634
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-07-09 15:49:00 +0200

    Brief Service

--- briefService/src/main/java/com/enaaskills/briefservice/Service/BriefService.java
+++ briefService/src/main/java/com/enaaskills/briefservice/Service/BriefService.java
@@ -1,10 +1,28 @@
 package com.enaaskills.briefservice.Service;
 
 
+import com.enaaskills.briefservice.Model.Brief;
+import com.enaaskills.briefservice.Repository.BriefRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BriefService {
+
+    private final BriefRepository briefRepository;
+
+    public BriefService(BriefRepository briefRepository) {
+        this.briefRepository = briefRepository;
+    }
+
+    public Brief creatBrief(Brief brief) {
+        return briefRepository.save(brief);
+    }
+
+    public List<Brief> getAllBriefs() {
+        return briefRepository.findAll();
+    }
 
 
 }
