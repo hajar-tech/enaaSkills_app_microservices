cf6df1924aee79177e17ef8a069be42e3d5c12b3
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-07-09 16:29:22 +0200

    tester les fonctionalites

--- briefService/src/main/java/com/enaaskills/briefservice/Model/Brief.java
+++ briefService/src/main/java/com/enaaskills/briefservice/Model/Brief.java
@@ -17,5 +17,35 @@ public class Brief {
     private LocalDate dateLimit;
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDateLimit() {
+        return dateLimit;
+    }
+
+    public void setDateLimit(LocalDate dateLimit) {
+        this.dateLimit = dateLimit;
+    }
 }
