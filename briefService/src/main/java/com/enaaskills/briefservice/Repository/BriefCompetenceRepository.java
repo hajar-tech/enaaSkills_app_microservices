97a66132548476e5e95c54be1678828182bf0373
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-07-10 11:39:52 +0200

    assigner une competence a un breif

---
+++ briefService/src/main/java/com/enaaskills/briefservice/Repository/BriefCompetenceRepository.java
@@ -0,0 +1,7 @@
+package com.enaaskills.briefservice.Repository;
+
+import com.enaaskills.briefservice.Model.BriefCompetence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BriefCompetenceRepository extends JpaRepository<BriefCompetence, Long> {
+}
