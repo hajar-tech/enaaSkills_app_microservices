b2d12d359df794b0a8adbdffdf955b4e2d2ad572
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-07-24 10:33:49 +0200

    add Dockerfile

--- authService/src/main/java/com/enaaskills/authservice/dtos/RegisterRequest.java
+++ authService/src/main/java/com/enaaskills/authservice/dtos/RegisterRequest.java
@@ -1,5 +1,11 @@
 package com.enaaskills.authservice.dtos;
 
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record RegisterRequest(
         String nom,
         String email,
